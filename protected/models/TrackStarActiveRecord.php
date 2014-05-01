7e34afa6d1aef940ef899b5eb09e34426da3e620
camilo reynaga <camilo.reynaga@gmail.com>
2014-05-01 02:44:52 -0500

    first commit

---
+++ protected/models/TrackStarActiveRecord.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+abstract class TrackStarActiveRecord extends CActiveRecord
+{
+    /**
+     * prepares create_time, create_user_id, update_time, and update_user_id attributes before performing validation
+     * 
+     */
+    protected function beforeValidate() {
+        if($this->isNewRecord)
+        {
+            //set the created date, last update date and the usser doing  the creacting
+            $this->create_time= $this->update_time=new CDbException('NOW()');
+            $this->create_user_id=  $this->update_user_id=  Yii::app()->user->id;
+                       
+        }
+       else
+       {
+           // not a  new record, so just set the last update time and the last update user
+           $this->update_time= new CDbExpression ('NOW()');
+           $this->update_user_id= Yii::app()->user->id;
+       }
+       return parent::beforeValidate();
+           
+      
+           
+    }
+    
+            
+}
+?>
